0af11561dad4002baccc225986c26ad69accd228
lkjtzsq <879932397@qq.com>
2020-02-26 19:45:40 +0800

    123

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,7 +8,51 @@ Page({
     userInfo: {},
     textTxt:'你好',
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    loading:false
+  },
+  showError:function(){
+    wx.showModal({
+
+      title: '标题',
+
+      content: '告知当前状态，信息和解决方法',
+
+      confirmText: '主操作',
+
+      cancelText: '次要操作',
+
+      success: function (res) {
+
+        if (res.confirm) {
+
+          console.log('用户点击主操作')
+
+        } else if (res.cancel) {
+
+          console.log('用户点击次要操作')
+
+        }
+
+      }
+
+    })
+  },
+  tap:function(){
+    var _this=this
+    this.setData({
+
+      loading: true
+
+    })
+    setTimeout(function(){
+      _this.setData({
+
+        loading: false
+
+      })
+    },2000)
+
   },
   //事件处理函数
   bindViewTap: function() {
@@ -17,6 +61,19 @@ Page({
     })
   },
   onLoad: function () {
+   /* wx.showLoading({
+      title: '22',
+    })*/
+    wx.showToast({ // 显示Toast
+
+      title: '已发送',
+
+      icon: 'success',
+
+      duration: 1500
+
+    })
+
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
